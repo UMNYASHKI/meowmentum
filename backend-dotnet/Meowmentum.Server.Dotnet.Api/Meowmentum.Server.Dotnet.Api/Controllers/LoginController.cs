6b7068d833d04417395333d46ee8a7291a3c6e43
NureRazinkovYevhenii <yevhenii.razinkov@nure.ua>
2024-11-04 15:09:45 +0200

    Add login with JWT

---
+++ backend-dotnet/Meowmentum.Server.Dotnet.Api/Meowmentum.Server.Dotnet.Api/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Meowmentum.Server.Dotnet.Business.Abstractions;
+using Meowmentum.Server.Dotnet.Shared.Requests;
+using Meowmentum.Server.Dotnet.Shared.Results;
+using Meowmentum.Server.Dotnet.Shared.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace Meowmentum.Server.Dotnet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login([FromBody] Shared.Requests.LoginRequest loginRequest, CancellationToken token = default)
+        {
+            var loginResult = await _loginService.LoginAsync(loginRequest, token);
+            if (!loginResult.IsSuccess)
+            {
+                return BadRequest(new { Message = loginResult.ErrorMessage });
+            }
+
+            return Ok(new LoginResponse { Token = loginResult.Data });
+        }
+    }
+}
---
+++ backend-dotnet/Meowmentum.Server.Dotnet.Api/Meowmentum.Server.Dotnet.Infrastructure/Implementations/LoginService.cs
@@ -0,0 +1,35 @@
+﻿using Meowmentum.Server.Dotnet.Business.Abstractions;
+using Meowmentum.Server.Dotnet.Core.Entities;
+using Meowmentum.Server.Dotnet.Shared.Requests;
+using Meowmentum.Server.Dotnet.Shared.Results;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Meowmentum.Server.Dotnet.Infrastructure.Implementations
+{
+    public class LoginService : ILoginService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _jwtService;
+
+        public LoginService(UserManager<AppUser> userManager, ITokenService jwtService)
+        {
+            _userManager = userManager;
+            _jwtService = jwtService;
+        }
+
+        public async Task<Result<string>> LoginAsync(Shared.Requests.LoginRequest request, CancellationToken token = default)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, request.Password))
+            {
+                return Result.Failure<string>("Invalid email or password.");
+            }
+
+            var tokenString = _jwtService.GetToken(user);
+            return Result.Success(tokenString);
+        }
+    }
+}
