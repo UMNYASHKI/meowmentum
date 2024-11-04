ff5dd65ec2181cb6ccf6a36bf1e7e7da9f7aa5a2
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-11-05 00:10:25 +0200

    add error handling middleware

---
+++ backend-dotnet/Meowmentum.Server.Dotnet.Api/Meowmentum.Server.Dotnet.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Meowmentum.Server.Dotnet.Shared.Responses;
+using System;
+using System.Data;
+using System.Net;
+using System.Security.Authentication;
+
+namespace Meowmentum.Server.Dotnet.Api.Middleware;
+
+public class ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            await WriteErrorToResponseAsync(context.Response, e, logger, $"{context.Request.Method}: {context.Request.Path}", context.RequestAborted);
+        }
+    }
+
+    private static async Task WriteErrorToResponseAsync(HttpResponse httpResponse, Exception exception, ILogger logger, string actionName, CancellationToken ct = default)
+    {
+        logger.LogError(exception, $"An unexpected error occurred at {actionName}.");  
+
+        ExceptionResponse response = exception switch
+        {
+            ArgumentNullException _ => new ExceptionResponse(HttpStatusCode.BadRequest, "Application exception occurred."),
+            KeyNotFoundException _ => new ExceptionResponse(HttpStatusCode.NotFound, "The request key not found."),
+            UnauthorizedAccessException _ => new ExceptionResponse(HttpStatusCode.Unauthorized, "Unauthorized."),
+            _ => new ExceptionResponse(HttpStatusCode.InternalServerError, "Internal server error.")
+        };
+
+        httpResponse.ContentType = "application/json";
+        httpResponse.StatusCode = (int)response.StatusCode;
+        await httpResponse.WriteAsJsonAsync(response, ct);
+    }
+}
