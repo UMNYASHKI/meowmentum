6c416b004c5904e14ac33b8afcee77b4376541db
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-11-04 21:29:44 +0200

    add redis cache service

---
+++ backend-dotnet/Meowmentum.Server.Dotnet.Api/Meowmentum.Server.Dotnet.Persistence/Abstractions/IRedisCacheService.cs
@@ -0,0 +1,11 @@
+﻿using Meowmentum.Server.Dotnet.Shared.Results;
+
+namespace Meowmentum.Server.Dotnet.Persistence.Abstractions;
+public interface IRedisCacheService
+{
+    Task<Result<bool>> ExistsAsync(string key, int? dbNum = -1, CancellationToken ct = default);
+    Task<Result<T>> GetAsync<T>(string key, int? dbNum = -1, CancellationToken ct = default);
+    Task<Result<bool>> RemoveAsync(string key, int? dbNum = -1, CancellationToken ct = default);
+    Task<Result<bool>> SetAsync<T>(string key, T value, TimeSpan expiration, int? dbNum = -1, bool overrideValeIfKeyExists = false, CancellationToken ct = default);
+    Task<Result<bool>> SetAsync<T>(string key, T value, int? dbNum = -1, bool overrideValeIfKeyExists = false, CancellationToken ct = default);
+}
