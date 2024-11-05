8e1ef650e3a79f38ae9b9514d58b33dbcabe99a7
Kostiantyn Shcherbyna <kostiantyn.shcherbyna@nure.ua>
2024-11-05 19:42:14 +0200

    task-3: password reset + refactoring

---
+++ backend/go/email/internal/service/method-password-reset.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"meowmentum/backend/email/internal/templates"
+	pbEmail "meowmentum/backend/proto/email"
+)
+
+type PasswordResetProps struct {
+	Name string
+	Code string
+}
+
+func (s *emailServiceServer) PasswordReset(_ context.Context, req *pbEmail.PasswordResetRequest) (*pbEmail.PasswordResetResponse, error) {
+	slog.Debug("email grpc: password reset", slog.Any("request", req))
+
+	buffer := &bytes.Buffer{}
+
+	err := templates.PasswordResetTemplate.Execute(buffer, PasswordResetProps{
+		Name: req.Name,
+		Code: req.ConfirmationCode,
+	})
+	if err != nil {
+		slog.Error("failed to execute password reset template", slog.Any("error", err))
+		return &pbEmail.PasswordResetResponse{}, nil
+	}
+
+	err = s.sender.SendEmail(req.Email, "Registration Confirmation", buffer.String())
+	if err != nil {
+		slog.Error("failed to send password reset email", slog.Any("error", err))
+	}
+
+	return &pbEmail.PasswordResetResponse{}, nil
+}
