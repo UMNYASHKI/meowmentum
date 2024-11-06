d126714f8a094471ab41f4a3809b0e8e83d87d1d
Bilous Mykola <nick20031103@gmail.com>
2024-11-06 17:57:51 +0200

    Update MainApp.kt

--- mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/MainApp.kt
+++ mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/MainApp.kt
@@ -1,13 +1,12 @@
 package org.meowmentum.project
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import org.meowmentum.project.navigation.AppNavigation
 import org.meowmentum.project.theme.MainTheme
-
 
 @Composable
 fun MainApp() {
     MainTheme {
-
+        AppNavigation()
     }
 }
