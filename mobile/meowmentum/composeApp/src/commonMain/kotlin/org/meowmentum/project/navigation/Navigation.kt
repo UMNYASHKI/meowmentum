2e687d64f26b799e00a65f7e14601e9fafd20295
Bilous Mykola <nick20031103@gmail.com>
2024-11-06 17:43:21 +0200

    Update Navigation.kt

--- mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/navigation/Navigation.kt
+++ mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/navigation/Navigation.kt
@@ -1,22 +1,40 @@
 package org.meowmentum.project.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.NavigatorDisposeBehavior
 import cafe.adriel.voyager.transitions.SlideTransition
+import org.koin.compose.koinInject
+import org.meowmentum.project.domain.repository.AuthRepository
 import org.meowmentum.project.ui.screens.auth.login.LoginScreen
-import org.meowmentum.project.ui.screens.auth.register.RegisterScreen
-
-sealed class Screen {
-    object Login : Screen()
-    object Register : Screen()
-    object Home : Screen()
-    object ForgotPassword : Screen()
-    object ResetPassword : Screen()
-}
+import org.meowmentum.project.ui.screens.home.HomeScreen
 
 @Composable
 fun AppNavigation() {
-    Navigator(screen = LoginScreen()) {
-        SlideTransition(it)
+    val authManager = koinInject<AuthRepository>()
+    val isLoggedIn by authManager.isUserLoggedIn().collectAsState(initial = false)
+
+    Navigator(
+        screen = if (isLoggedIn) HomeScreen() else LoginScreen(),
+        disposeBehavior = NavigatorDisposeBehavior(
+            disposeNestedNavigators = false,
+            disposeSteps = true
+        )
+    ) { navigator ->
+        SlideTransition(navigator)
     }
+}
+
+sealed class Screen {
+    data class Auth(val route: AuthRoute) : Screen()
+    object Home : Screen()
+}
+
+sealed class AuthRoute {
+    object Login : AuthRoute()
+    object Register : AuthRoute()
+    object ForgotPassword : AuthRoute()
+    data class ResetPassword(val token: String) : AuthRoute()
 }
