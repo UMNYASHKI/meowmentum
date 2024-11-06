dafc0f5e3ee9a895bbd937298bc4766b71b19584
Bilous Mykola <nick20031103@gmail.com>
2024-11-06 15:26:35 +0200

    update theming

--- mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/theme/Typography.kt
+++ mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/theme/Typography.kt
@@ -1,2 +1,23 @@
 package org.meowmentum.project.theme
 
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AlbertSansTypography() = Typography(
+    displayLarge = TextStyle(
+        fontFamily = AlbertSansFontFamily(),
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = AlbertSansFontFamily(),
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+)
