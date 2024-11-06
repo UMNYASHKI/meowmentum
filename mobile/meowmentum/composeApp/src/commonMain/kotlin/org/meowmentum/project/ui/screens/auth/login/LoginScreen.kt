4e431b47d80bb2d616c0bfebd67bcc3e895ef672
Bilous Mykola <nick20031103@gmail.com>
2024-11-06 15:49:34 +0200

    Update LoginScreen.kt

--- mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/ui/screens/auth/login/LoginScreen.kt
+++ mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/ui/screens/auth/login/LoginScreen.kt
@@ -1,2 +1,10 @@
 package org.meowmentum.project.ui.screens.auth.login
 
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class LoginScreen() : Screen {
+    @Composable
+    override fun Content() {
+    }
+}
