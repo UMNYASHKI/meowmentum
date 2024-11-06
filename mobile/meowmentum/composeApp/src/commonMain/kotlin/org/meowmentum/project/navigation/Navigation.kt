c943f4dcbbf449a330653220a74b6392d33b4915
Bilous Mykola <nick20031103@gmail.com>
2024-11-06 15:14:55 +0200

    start for navigation

--- mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/navigation/Navigation.kt
+++ mobile/meowmentum/composeApp/src/commonMain/kotlin/org/meowmentum/project/navigation/Navigation.kt
@@ -1,2 +1,22 @@
 package org.meowmentum.project.navigation
 
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
+import org.meowmentum.project.ui.screens.auth.login.LoginScreen
+import org.meowmentum.project.ui.screens.auth.register.RegisterScreen
+
+sealed class Screen {
+    object Login : Screen()
+    object Register : Screen()
+    object Home : Screen()
+    object ForgotPassword : Screen()
+    object ResetPassword : Screen()
+}
+
+@Composable
+fun AppNavigation() {
+    Navigator(screen = LoginScreen()) {
+        SlideTransition(it)
+    }
+}
