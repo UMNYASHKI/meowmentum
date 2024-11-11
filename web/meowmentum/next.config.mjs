f4481ff64e2f8f4101f86fc22cd979d51b388382
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-11-11 22:34:25 +0200

    task-29: add svg components

--- web/meowmentum/next.config.mjs
+++ web/meowmentum/next.config.mjs
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const nextConfig = ({
   output: "standalone",
   env: {
     NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-  }
-};
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+});
 
 export default nextConfig;
