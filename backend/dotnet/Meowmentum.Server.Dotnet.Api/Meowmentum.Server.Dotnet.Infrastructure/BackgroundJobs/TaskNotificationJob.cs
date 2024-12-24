c7d1373a88d460d97fe168f53eae7490786abddc
Nikita Shvets <ns.shvets@gmail.com>
2024-12-24 13:00:59 +0100

    add task notification job

---
+++ backend/dotnet/Meowmentum.Server.Dotnet.Api/Meowmentum.Server.Dotnet.Infrastructure/BackgroundJobs/TaskNotificationJob.cs
@@ -0,0 +1,27 @@
+﻿using Meowmentum.Server.Dotnet.Business.Abstractions;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace Meowmentum.Server.Dotnet.Infrastructure.BackgroundJobs;
+
+public class TaskNotificationJob(
+    INotificationService notificationService, 
+    ILogger<TaskNotificationJob> logger) : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        try
+        {
+            logger.LogInformation("Executing TaskNotificationJob...");
+
+            await notificationService.NotifyAboutUpcomingTasksAsync(context.CancellationToken);
+
+            await notificationService.NotifyAboutOverdueTasksAsync(context.CancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error occurred while executing TaskNotificationJob.");
+        }
+    }
+}
+
